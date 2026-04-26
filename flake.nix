663d82ef8c733e6e711abb26be143d79e2649695
Millrocious <79750098+Millrocious@users.noreply.github.com>
2026-04-26 19:30:34 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs =
-    { nixpkgs }:
+    { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
